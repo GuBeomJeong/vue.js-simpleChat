a496e933cd003d0903fdd2f246ce440299e29d0c
GuBeomJeong <jgb625@naver.com>
2017-11-05 22:12:55 +0900

    init

---
+++ component/test.vue
@@ -0,0 +1,19 @@
+
+<template>
+    <div class="example">{{ message }}</div>
+</template>
+
+<script>
+    export default {
+        props: ["message"],
+        data : function() {
+            return {}
+        }
+    }
+</script>
+
+<style>
+    .example {
+        color: blue;
+    }
+</style>
